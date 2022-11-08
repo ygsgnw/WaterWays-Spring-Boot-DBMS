37b0619df2db1aba5833e3ba25ab8674ae248b07
koderkushy <1kushagra1@gmail.com>
2022-11-08 20:48:19 +0530

    schema

--- src/main/resources/CrewInsertTrigger.sql
+++ src/main/resources/CrewInsertTrigger.sql
@@ -6,16 +6,21 @@ delimiter $
 create trigger CrewInsertTrigger before insert
     on Crew for each row
 begin
-    if exists (
-        SELECT dtime, atime FROM (
-            SELECT DepartureTime as dtime, ArrivalTime as atime FROM Crew, Voyage
-            WHERE Crew.EmployeeId = @EmployeeId AND Crew.VoyageId = Voyage.VoyageId AND Voyage.ArrivalTime > NOW()
-        ) as da
-        WHERE
-            NOT(@ArrivalTime < dtime OR @DepartureTime > atime)
-    ) OR exists (
-        
-    ) THEN
+
+    if (
+        (SELECT EmployeeStatusCode FROM Employee WHERE Employee.EmployeeId = NEW.EmployeeId) = 2
+        or
+        exists(
+            SELECT * FROM (
+                SELECT Voyage.DepartureTime as dtime, Voyage.ArrivalTime as atime FROM Crew INNER JOIN Voyage
+                ON Crew.EmployeeId = NEW.EmployeeId AND Crew.VoyageId = Voyage.VoyageId
+            ) as WillServe, (
+                SELECT DepartureTime as dtime, ArrivalTime as atime FROM Voyage WHERE Voyage.VoyageId = NEW.VoyageId
+            ) as NewVoyage
+            WHERE
+                WillServe.atime > NewVoyage.dtime AND WillServe.dtime < NewVoyage.atime
+        )
+    ) then
         signal sqlstate '45000' set message_text = 'Employee serves on another voyage at the same time';
     END IF;
 end;
